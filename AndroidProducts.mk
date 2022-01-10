1de05b38354d01a965ca9cdb6883ba2e8439989b
kidnappedmyself1 <97450347+kidnappedmyself1@users.noreply.github.com>
2022-01-10 18:36:44 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_sweet.mk
+    $(LOCAL_DIR)/corvus_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_sweet-eng \
-    aosp_sweet-userdebug \
-    aosp_sweet-user
+    corvus_sweet-eng \
+    corvus_sweet-userdebug \
+    corvus_sweet-user
